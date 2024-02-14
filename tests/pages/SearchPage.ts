87b7f4e2643edd9a812ee950d3791c832e098989
Daniel Fabian <dafl16@hotmail.com>
2024-02-13 18:28:08 -0600

    Add 2 test cases about search repo

--- tests/pages/SearchPage.ts
+++ tests/pages/SearchPage.ts
@@ -4,11 +4,15 @@ export class SearchPage{
     readonly  _userNameInput: Locator;
     readonly _goButton: Locator;
     readonly page: Page;
+    readonly _messageFailure: Locator;
+    readonly _messageSuccess: Locator;
 
     constructor(page: Page){
         this.page = page;
         this._userNameInput = page.locator('#username');
         this._goButton = page.locator('.submit');
+        this._messageFailure = page.locator('.message-failure');
+        this._messageSuccess = page.locator('.message-success');
     }
 
     async fillUsername(username: string){
--- tests/test-flows.spec.ts
+++ tests/test-flows.spec.ts
@@ -6,11 +6,39 @@ import { SearchPage } from './pages/SearchPage'
 
 const usernames = [generateUsername('-'), generateUsername('',3),generateUsername("", 3, 19)]
 for(const username of usernames){
-  test(`Search multiple usernames: Username ${username}`, async ({ page }) => {
+  test(`Search multiple invalid usernames: Username ${username}`, async ({ page }) => {
     await page.goto(BASE_URL);
     let searchPage = new SearchPage(page);
     await searchPage.fillUsername(username);
     await searchPage._goButton.hover();
     await searchPage.clickOnGoBtn();
+    await expect(searchPage._messageFailure).toBeVisible();
   });
 }
+
+const validUsers = ['hello', 'test', 'issue', 'bug']
+for(const validUser of validUsers){
+  test(`Search multiple usernames: Username ${validUser}`, async ({ page }) => {
+    await page.goto(BASE_URL);
+    let searchPage = new SearchPage(page);
+    await searchPage.fillUsername(validUser);
+    await searchPage._goButton.hover();
+    await searchPage.clickOnGoBtn();
+    await expect(searchPage._messageSuccess).toBeVisible();
+  });
+}
+
+
+test(`Verify the link on the result is the right repo`, async ({ page,context }) => {
+  await page.goto(BASE_URL);
+  let searchPage = new SearchPage(page);
+  await searchPage.fillUsername('bug');
+  await searchPage._goButton.hover();
+  await searchPage.clickOnGoBtn();
+  let resultLinks = await page.getByRole('link');
+  let expectedRepo = resultLinks[0].textContent();
+  await page.click(resultLinks[0]);
+  const newPage = await context.WaitForEvent('page');
+  await newPage.waitForLoadState();
+  await expect(newPage.url()).toContain(expectedRepo);
+});
